ede27ca89c3df2cb1948feaafd10008543a7d2cd
syspro86 <syspro86@gmail.com>
2025-07-28 15:40:10 +0000

    .

--- spring/src/test/java/net/zsoo/spring/mybatis/MybatisServiceTest.java
+++ spring/src/test/java/net/zsoo/spring/mybatis/MybatisServiceTest.java
@@ -1,11 +1,13 @@
 package net.zsoo.spring.mybatis;
 
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 
 @SpringJUnitWebConfig(locations = {
-        // "file:src/main/webapp/WEB-INF/spring/servlet-context.xml",
         "classpath:properties/application-context.xml",
 })
 public class MybatisServiceTest {
@@ -16,9 +18,8 @@ public class MybatisServiceTest {
     @Test
     public void testGetAllRecords() {
         Record[] records = mybatisService.getAllRecords();
-        // Add assertions to verify the records
-        assert records != null : "Records should not be null";
-        assert records.length > 0 : "There should be at least one record";
-        // Further assertions can be added based on expected data
+
+        assertNotNull(records, "Records should not be null");
+        assertNotEquals(0, records.length, "Records should not be empty");
     }
 }
